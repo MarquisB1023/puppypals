fe8e595611b5f290743188867a02afdc623a0178
marquis brunbdage <marquisbrunbdage@marquiss-MacBook-Pro.local>
2024-01-29 16:50:06 -0500

    finished puppypalassigment

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { puppyList } from "./data";
+
+export default function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  function handleClick(id) {
+    setFeatPupId(id);
+  }
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  return (
+    <div className="App">
+      {puppies.map((puppy) => {
+        return (
+          <p onClick={() => handleClick(puppy.id)} key={puppy.id}>
+            {puppy.name}
+          </p>
+        );
+      })}
+
+      {featuredPup && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
